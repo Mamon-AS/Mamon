785c0227c7663b7b6bdaf4cd962a719d034339eb
Martin Singstad <40832199+msingstad@users.noreply.github.com>
2022-11-24 14:52:06 +0000

    initialize Sanity

---
+++ mamon/schemas/marketing.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'marketing',
+    type: 'document',
+      title: 'Marketing',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title'
+      },
+      {
+        name: 'image',
+        type: 'image',
+        title: 'Image'
+      },
+      {
+        name: 'body',
+        type: 'text',
+        title: 'Body'
+      },
+    ]
+  }
