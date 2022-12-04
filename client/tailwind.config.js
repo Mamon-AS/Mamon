1a4fb318ded0b8d48799ae0383cdcb2656f0da2c
Erik S <44195327+stjernando@users.noreply.github.com>
2022-12-04 21:34:43 +0100

    Adding a temp header, fixing tailwind (#9)
    Adding a header mostly for checking vue components and app.
    Fixing tailwind, did not work on my local and I assume it did not work elsewhere.
    Array type in header in Sanity was missing required "of", arrays must be specified what the arrays are of. I assumed it was supposed to to show companies.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,html,js,tsx}"],
   theme: {
     extend: {},
   },
