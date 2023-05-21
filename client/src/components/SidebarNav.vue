db7b542dbc84ed779e959b17441eb65480aeb8f2
stjernando <erikstjern@hotmail.com>
2023-05-21 22:44:06 +0200

    adding a homepage, messing around with waves

--- client/src/components/SidebarNav.vue
+++ client/src/components/SidebarNav.vue
@@ -1,23 +1,23 @@
 <template>
     <aside
-        :class="`fixed top-0 left-0 min-h-screen pt-16 z-40 w-full max-w-xs transform duration-300 bg-gray-800 ${
+        :class="`fixed top-0 left-0 min-h-screen pt-16 z-40 w-full max-w-xs transform duration-300 bg-orange-200 ${
             menu_is_active
             ? 'translate-x-0'
             : '-translate-x-full'
         }`">
         <div class="p-4">
-            <h2 class="text-gray-500 text-sm font-bold uppercase mb-2">
-                Menu
+            <h2 class="text-gray-100 text-lg font-bold uppercase mb-2">
+                Meny
             </h2>
             <!-- Ikoner og div fra https://fonts.google.com/icons -->
             <div class="mx-4">
                 <MenuItem to="/">
                 <span class="material-icons mr-2">home</span>
-                    Home
+                    Hjem
                 </MenuItem>
-                <MenuItem to="/">
-                    <span class="material-icons mr-2">domain</span>
-                    Companies
+                <MenuItem to="/card">
+                    <span class="material-icons mr-2 ">domain</span>
+                    Annonser
                 </MenuItem>
             </div>
             
