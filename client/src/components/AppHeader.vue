fc652162dc608361f6028ce059bd18b71c1767bd
Erik S <44195327+stjernando@users.noreply.github.com>
2023-07-25 15:40:26 +0200

    User login (#28)
    * Login google
    
    * login google

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -11,39 +11,119 @@
         <div class="hamburger">
           <span></span>
         </div>
-
       </div>
+
      
-      <div class="flex items-center ml-auto">
+      <div v-if="!loggedIn" class="flex items-center ml-auto">
         <h1 class="text-white text-xl font-semibold ml-10">Mamon</h1>
         <a href="/"> 
           <img src="../images/High_Resolution_Image_11.jpg" alt="Logo" class="h-8 w-auto logo">
         </a>
       </div>
+      
+      <div v-if="loggedIn" class="flex items-center ml-auto">
+      <div class="flex items-center ml-4">
+        <h2 class="text-white text-xl font-semibold">{{ user.name }}</h2>
+        <img :src="user.picture" :alt="user.name" class="h-10 w-10 rounded-full ml-2" />
+      </div>
+      <button @click="logout" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-4 rounded">
+        Logout
+      </button>
+    </div>
 
+      <div v-else>
+        <button id="loginButton" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-4 rounded">
+          Ny her?
+          <span class="material-icons mr-2">account_circle</span>
+        </button>
+      </div>
   </header>
+  
+<div class="modal" id="loginModal">
+  <div class="modal-content">
+    <!--  prompt auto-login -->
+    <GoogleLogin :callback="callback" prompt auto-login/>
+  </div>
+</div>
 </template>
 
 <script>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import { decodeCredential, googleLogout } from 'vue3-google-login'
 
 export default {
+  data() {
+    return {
+      loggedIn: false,
+      user:null,
+
+      callback:(response) => {
+          console.log("logged in");
+          this.loggedIn = true;
+          this.user = decodeCredential(response.credential);
+
+          const loginModal = document.getElementById('loginModal');
+          loginModal.style.display = 'none';
+      }
+    }
+  },
+  methods: {
+    logout() {
+      googleLogout();
+      this.loggedIn = false;
+    }
+  },
   setup() {
       const store = useStore()
 
       const ToggleMenu = () => store.dispatch('ToggleMenu')
       
-      
       return {
         menu_is_active: computed(() => store.state.menu_is_active),
         ToggleMenu
       }
-  }
-}
-</script>
+  },
+  mounted() {
+    // JavaScript to show/hide the modal
+    const loginButton = document.getElementById('loginButton');
+    const loginModal = document.getElementById('loginModal');
 
+    loginButton.addEventListener('click', () => {
+      loginModal.style.display = 'block';
+    });
+
+    // Close the modal when the user clicks anywhere outside of it
+    window.addEventListener('click', (event) => {
+      if (event.target === loginModal) {
+        loginModal.style.display = 'none';
+      }
+    });
+  },
+};
+</script>
 <style scoped>
+ 
+  .modal {
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+  }
+
+ 
+  .modal-content {
+    background-color: #f4f4f4;
+    margin: 10% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 300px;
+    border-radius: 5px;
+  }
 
 .logo {
   margin-left: auto;
--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vue3GoogleLogin from 'vue3-google-login'
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
 import './assets/main.css'
 
@@ -9,4 +12,6 @@ const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(vue3GoogleLogin, {
+    clientId: GOOGLE_CLIENT_ID}),
 app.mount('#app')
