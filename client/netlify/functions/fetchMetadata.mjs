1db9c5e820bf868c122e5afe51193a0b88e99bc1
Erik S <44195327+stjernando@users.noreply.github.com>
2024-02-03 13:37:07 +0100

    proxy image serverside instead (#60)

--- client/netlify/functions/fetchMetadata.mjs
+++ client/netlify/functions/fetchMetadata.mjs
@@ -1,29 +1,44 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
+import { Buffer } from 'buffer'
 
 exports.handler = async (event) => {
     try {
-        const data = JSON.parse(event.body);
-        const url = data.url;
-
+        const { url } = JSON.parse(event.body);
         console.log("Fetching data from url: " + url + " ...");
-
-
         const response = await axios.get(url);
         const body = response.data;
         const $ = cheerio.load(body);
+
         const title = $('meta[property="og:title"]').attr('content') || $('title').text();
-        const image = $('meta[property="og:image"]').attr('content')
+        const imageUrl = $('meta[property="og:image"]').attr('content')
+
+        const imageResponse = await axios({
+              method: 'GET',
+              url: imageUrl,
+              responseType: 'arraybuffer' 
+        });
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ title, image })
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*' 
+            },
+            body: JSON.stringify({
+              title,
+              image: `data:${imageResponse.headers['content-type']};base64,${Buffer.from(imageResponse.data).toString('base64')}`
+          })
         };
-     } catch (error) {
-        console.error("Error in function:", error);
-      return { 
-        statusCode: 500, 
-        body: `Error: ${error.message}` 
-      };
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+            statusCode: 500,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({ error: 'Failed to fetch metadata' })
+        };
     }
-  };
+};
