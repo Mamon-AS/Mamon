7d6e47fe686939dc479181998367735f2a13a5b9
Martin Singstad <40832199+msingstad@users.noreply.github.com>
2024-03-07 10:51:21 +0100

    Change author on comments (#96)
    * nødhjelp
    
    * Make cards more lightweight and compact
    
    * Make reply comments up to date
    
    * Show reactions (more) properly
    
    * make comment text smaller
    
    * Change author on comments
    
    ---------
    
    Co-authored-by: Martin Singstad <Martin.Singstad@norwegian.com>

--- client/src/components/Comments/CommentItem.vue
+++ client/src/components/Comments/CommentItem.vue
@@ -1,47 +1,42 @@
 <template>
-  <div>
-    <!-- Main comment -->
-    <div class="comment-item p-4 border-b border-gray-200">
-      <img :src="props.photoUrl" alt="Profile picture" class="object-cover rounded-full w-10 h-10 border-4 border-gray-800 mr-2 cursor-pointer"
-              @click="navigate(props.userId)" />
-      <div class="author" @click="navigate(props.userId)">
-        <span class="hover:underline cursor-pointer">{{ props.displayName }}</span>
-      </div> 
-      <div class="text-s mt-1">{{ props.text }}</div>
-      <div class="details mt-2 text-sm text-gray-500">
-        <span class="date">{{ date }}</span>
-        <!-- Add a reply button or link -->
-        <span class="reply-link text-blue-500 cursor-pointer ml-4" @click="showReplyInput = !showReplyInput">Svar</span>
-        <span v-if="props.userId === currentUserId" class="delete-link text-red-500 cursor-pointer ml-4" @click="deleteComment(props.commentId)">Slett</span>
-       </div>
-      <!-- Reply input field -->
-      <div v-if="showReplyInput" class="mt-2">
-        <textarea v-model="replyText" placeholder="Skriv et svar.." class="textarea w-full p-2 border rounded-md border-gray-300"></textarea>
-        <button @click="postReply" class="submit-btn bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 mt-2">Svar</button>
+  <!-- Main comment -->
+  <div class="comment-item p-4 border-b border-gray-200">
+    <div class="author" @click="navigate(props.userId)">
+      <img :src="props.photoUrl" alt="Profile picture" class="object-cover rounded-full w-10 h-10 border-4 border-gray-800 mr-2 cursor-pointer"/>
+      <span class="hover:underline cursor-pointer">{{ props.displayName }}</span>
+    </div>
+    <div class="text-s mt-1">{{ props.text }}</div>
+    <div class="details mt-2 text-sm text-gray-500">
+      <span class="date">{{ date }}</span>
+      <!-- Add a reply button or link -->
+      <span class="reply-link text-blue-500 cursor-pointer ml-4" @click="showReplyInput = !showReplyInput">Svar</span>
+      <span v-if="props.userId === currentUserId" class="delete-link text-red-500 cursor-pointer ml-4" @click="deleteComment(props.commentId)">Slett</span>
+    </div>
+    <!-- Reply input field -->
+    <div v-if="showReplyInput" class="mt-2">
+      <textarea v-model="replyText" placeholder="Skriv et svar.." class="textarea w-full p-2 border rounded-md border-gray-300"></textarea>
+      <button @click="postReply" class="submit-btn bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 mt-2">Svar</button>
+    </div>
+  </div>
+
+  <!-- Replies to the comment -->
+  <div class="replies pl-8 mt-2 mb-2" v-if="props.replies && props.replies.length">
+    <div v-for="reply in props.replies" :key="reply.commentId" class="reply-item p-2 border-b border-gray-100">
+      <div class="author" @click="navigate(reply.userId)">
+        <img :src="reply.photoUrl" alt="Profile picture" class="object-cover rounded-full w-8 h-8 border-2 border-gray-700 mr-2 cursor-pointer"/>
+        <span class="hover:underline cursor-pointer">{{ reply.displayName }}</span>
+      </div>
+      <div class="text-s mt-1">{{ reply.text }}</div>
+      <div class="details mt-2 text-xs text-gray-500">
+        <span class="date">{{ timeStampToDate(reply.createdAt) }}</span>
+        <span class="reply-link text-blue-500 cursor-pointer ml-4" @click="showReplyToReplyInput = reply.commentId">Svar</span>
+        <span v-if="reply.userId === currentUserId" class="delete-link text-red-500 cursor-pointer ml-4" @click="deleteComment(reply.commentId)">Slett</span>
+        <div v-if="showReplyToReplyInput === reply.commentId" class="mt-2">
+            <textarea v-model="replyText" placeholder="Skriv et svar.." class="textarea w-full p-2 border rounded-md border-gray-300"></textarea>
+            <button @click="() => postReply(reply.commentId)" class="submit-btn bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 mt-2">Svar</button>
+        </div>
       </div>
     </div>
-
-    <!-- Replies to the comment -->
-    <div class="replies pl-8 mt-2 mb-2" v-if="props.replies && props.replies.length">
-      <div v-for="reply in props.replies" :key="reply.commentId" class="reply-item p-2 border-b border-gray-100">
-        <img :src="reply.photoUrl" alt="Profile picture" class="object-cover rounded-full w-8 h-8 border-2 border-gray-700 mr-2 cursor-pointer"
-                  @click="navigate(reply.userId)" />
-        <div class="author">
-          <span class="hover:underline cursor-pointer" @click="navigate(reply.userId)">{{ reply.displayName }}</span>
-        </div>
-        <div class="text-s mt-1">{{ reply.text }}</div>
-        <div class="details mt-2 text-xs text-gray-500">
-          <span class="date">{{ timeStampToDate(reply.createdAt) }}</span>
-          <span class="reply-link text-blue-500 cursor-pointer ml-4" @click="showReplyToReplyInput = reply.commentId">Svar</span>
-          <span v-if="reply.userId === currentUserId" class="delete-link text-red-500 cursor-pointer ml-4" @click="deleteComment(reply.commentId)">Slett</span>
-          <div v-if="showReplyToReplyInput === reply.commentId" class="mt-2">
-              <textarea v-model="replyText" placeholder="Skriv et svar.." class="textarea w-full p-2 border rounded-md border-gray-300"></textarea>
-              <button @click="() => postReply(reply.commentId)" class="submit-btn bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 mt-2">Svar</button>
-          </div>
-        </div>
-        </div>
-    </div>
-
   </div>
 </template>
 
