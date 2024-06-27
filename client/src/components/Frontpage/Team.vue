132888b6390771542a0ab56e94ea9306984ed5c5
stjernando <erikstjern@hotmail.com>
2024-06-28 00:43:42 +0200

    adding bulletpoints, and pics

--- client/src/components/Frontpage/Team.vue
+++ client/src/components/Frontpage/Team.vue
@@ -18,23 +18,23 @@
     {
       id: 1,
       name: 'Sondre',
-      role: 'Tittel',
-      image: 'https://via.placeholder.com/150',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      role: 'CEO',
+      image: '/images/sondre.jpg',
+      bio: 'Hjernen bak.'
     },
     {
       id: 2,
       name: 'Martin',
-      role: 'Tittel',
-      image: 'https://via.placeholder.com/150',
-      bio: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem.'
+      role: 'CTO',
+      image: '/images/martin.jpg',
+      bio: '"Liker å ha mange baller i luften".'
     },
     {
       id: 3,
       name: 'Erik',
-      role: 'Tittel',
-      image: 'https://via.placeholder.com/150',
-      bio: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      role: 'Webutvikler',
+      image: '/images/erik.jpg',
+      bio: 'Produserer og fjerner bugs'
     }
   ]);
   </script>
